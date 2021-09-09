dc608a4898255c5740359fae8ca21405487fbe9b
Pedro Augusto <pedro.marques@zup.com.br>
2021-09-09 16:09:35 -0300

    Teste Cadastro de Pizza = Pedro Augusto

--- src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
@@ -1,11 +1,14 @@
 package br.com.zup.edu.pizzaria.pizzas.cadastropizza;
 
+import br.com.zup.edu.pizzaria.ingredientes.Ingrediente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,7 +19,8 @@ class NovaPizzaControllerTest {
     private MockMvc mvc;
 
     void deveCadastrarUmaNovaPizza(){
-//        NovaPizzaRequest body = new NovaPizzaRequest()
+        Ingrediente ingrediente = new Ingrediente("Sei la", 2, new BigDecimal("2.0"));
+        NovaPizzaRequest body = new NovaPizzaRequest("Peperone", List.of(1L, 2L));
     }
 
 }
